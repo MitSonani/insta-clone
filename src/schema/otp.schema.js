ddaa3cb7533fbfc26d1283626d2db6fbeae05912
Mit <mitsonani7777@gmail.com>
2024-10-24 21:29:59 +0530

    Add Schema for followers and following

--- src/schema/otp.schema.js
+++ src/schema/otp.schema.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose')
 
 const otpSchema = new mongoose.Schema({
-    email: {
-        type: String
-    },
-    otp: {
-        type: String,
-        require: true
-    },
-    expiresIn: {
-        type: Date,
-        reuire: true
-    }
+    email: { type: String },
+    otp: { type: String, require: true },
+    expiresIn: { type: Date, require: true }
 }, { timestamps: true })
 
 const Otp = mongoose.model("Otp", otpSchema)
